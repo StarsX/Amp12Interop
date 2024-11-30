bad17bfbe7fd7972e61d538ed9fa09dff310797f
StarsX <X-Universe@outlook.com>
2024-11-30 11:08:18 +0800

    Fix auto adapter selection and refine timer

--- AmpDX12Interop/AmpDX12Interop.h
+++ AmpDX12Interop/AmpDX12Interop.h
@@ -39,6 +39,13 @@ public:
 	virtual void ParseCommandLineArgs(wchar_t* argv[], int argc);
 
 private:
+	enum DeviceType : uint8_t
+	{
+		DEVICE_DISCRETE,
+		DEVICE_UMA,
+		DEVICE_WARP
+	};
+
 	static const uint8_t FrameCount = 3;
 
 	XUSG::SwapChain::uptr			m_swapChain;
@@ -59,9 +66,10 @@ private:
 	uint64_t	m_fenceValues[FrameCount];
 
 	// Application state
+	DeviceType	m_deviceType;
+	StepTimer	m_timer;
 	bool		m_showFPS;
 	bool		m_isPaused;
-	StepTimer	m_timer;
 
 	// User external settings
 	std::wstring m_fileName;
